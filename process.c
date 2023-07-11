9ee325e24d2a3b3b7de8d6328299ea83b842306f
Igor Parfenov <parfenov_2001@mail.ru>
2023-07-11 18:03:10 +0300

    process

--- process.c
+++ process.c
@@ -31,7 +31,7 @@ struct Node *Parse(const char *filename) {
     return node;
 }
 
-void Assemble(const char *filename) {
+void Assemble(const char *input, const char *output) {
     unsigned long long pid = -1;
     asm("mov $0x39, %%rax\n"
         "syscall\n"
@@ -39,7 +39,7 @@ void Assemble(const char *filename) {
         : "=r"(pid));
     if (pid == 0) {
         const char *nasm = "/usr/bin/nasm";
-        const char *const args[] = {"/usr/bin/nasm", "-f", "elf64", concat(filename, ".asm"), "-o", concat(filename, ".o"), NULL};
+        const char *const args[] = {"/usr/bin/nasm", "-f", "elf64", input, "-o", output, NULL};
         asm("mov $0x3b, %%rax\n"
             "mov %0, %%rdi\n"
             "mov %1, %%rsi\n"
@@ -47,6 +47,35 @@ void Assemble(const char *filename) {
             "syscall\n"
             :
             : "r"(nasm), "r"(args)
+            : "%rax", "%rdi", "%rsi", "%rdx");
+    }
+    asm("mov $0x3d, %%rax\n"
+        "mov %0, %%rdi\n"
+        "mov $0, %%rsi\n"
+        "mov $0, %%rdx\n"
+        "mov $0, %%r10\n"
+        "syscall\n"
+        :
+        : "r"(pid)
+        : "%rax", "%rdi", "%rsi", "%rdx", "%r10");
+}
+
+void Link(const char *input, const char *output) {
+    unsigned long long pid = -1;
+    asm("mov $0x39, %%rax\n"
+        "syscall\n"
+        "mov %%rax, %0\n"
+        : "=r"(pid));
+    if (pid == 0) {
+        const char *gcc = "/usr/bin/ld";
+        const char *const args[] = {"/usr/bin/ld", input, "-o", output, NULL};
+        asm("mov $0x3b, %%rax\n"
+            "mov %0, %%rdi\n"
+            "mov %1, %%rsi\n"
+            "mov $0, %%rdx\n"
+            "syscall\n"
+            :
+            : "r"(gcc), "r"(args)
             : "%rax", "%rdi", "%rsi", "%rdx");
     }
     asm("mov $0x3d, %%rax\n"
@@ -71,62 +100,52 @@ int Process(struct Settings *settings) {
     const char *cmd;
     if (settings->compile || settings->assemble || settings->link) {
         const char *filename = settings->inputFilename;
-        const char *output_filename = NULL;
+        const char *process_filename = NULL;
         for (int i = 0; filename[i] != '\0'; i++) {
             if (filename[i] == '.') {
-                output_filename = substr(filename, i);
+                process_filename = substr(filename, i);
                 break;
             }
         }
 
-        char *str = concat(output_filename, ".asm");
+        char *str;
+        if (!settings->assemble && !settings->link && settings->outputFilename) {
+            str = _strdup(settings->outputFilename);
+        }
+        else {
+            str = concat(process_filename, ".asm");
+        }
         FILE *file = fopen(str, "w");
         _free(str);
         Compile(node, file, settings);
         fclose(file);
         if (settings->assemble || settings->link) {
-            Assemble(output_filename);
-            
-            
-        }
-        /*if (Settings::GetAssemble() || Settings::GetLink()) {
-            cmd = "nasm -f elf32 " + filename + ".asm -o " + filename + ".o";
-            system(cmd.c_str());
-
-            if (Settings::GetLink()) {
-                cmd = "gcc -m32 " + filename + ".o -no-pie -o " + filename;
-                system(cmd.c_str());
-
-                cmd = "rm " + filename + ".asm";
-                system(cmd.c_str());
-
-                cmd = "rm " + filename + ".o";
-                system(cmd.c_str());
-
-                std::string output_filename = Settings::GetOutputFilename();
-                if (!output_filename.empty() && filename != output_filename) {
-                    cmd = "mv " + filename + " " + output_filename;
-                    system(cmd.c_str());
-                }
+            char *str1, *str2;
+            str1 = concat(process_filename, ".asm");
+            if (!settings->link && settings->outputFilename) {
+                str2 = _strdup(settings->outputFilename);
             }
             else {
-                cmd = "rm " + filename + ".asm";
-                system(cmd.c_str());
-                
-                std::string output_filename = Settings::GetOutputFilename();
-                if (!output_filename.empty() && filename + ".o" != output_filename) {
-                    cmd = "mv " + filename + ".o " + output_filename;
-                    system(cmd.c_str());
+                str2 = concat(process_filename, ".o");
+            }
+            Assemble(str1, str2);
+            _free(str1);
+            _free(str2);
+
+            if (settings->link) {
+                char *str1, *str2;
+                str1 = concat(process_filename, ".o");
+                if (!settings->link && settings->outputFilename) {
+                    str2 = _strdup(settings->outputFilename);
                 }
+                else {
+                    str2 = _strdup(process_filename);
+                }
+                Link(str1, str2);
+                _free(str1);
+                _free(str2);
             }
         }
-        else {
-            std::string output_filename = Settings::GetOutputFilename();
-            if (!output_filename.empty() && filename + ".asm" != output_filename) {
-                cmd = "mv " + filename + ".asm " + output_filename;
-                system(cmd.c_str());
-            }
-        }*/
     }
 
     return 0;
