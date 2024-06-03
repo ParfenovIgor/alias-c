4f6c1634204a21b3cec6afa920a5820fabc77c79
Igor Parfenov <parfenov_2001@mail.ru>
2024-06-04 00:04:20 +0300

    syntax highlighting upgrade

--- include/lexer.h
+++ include/lexer.h
@@ -4,3 +4,4 @@
 #include "../include/token.h"
 
 struct TokenStream *Lexer_Process(const char *str, const char *filename);
+const char *Lexer_Highlight(const char *str);
--- src/languageserver.c
+++ src/languageserver.c
@@ -3,18 +3,8 @@
 #include "../include/lexer.h"
 #include "../stdlib/include/string.h"
 
-#define BUFFER_SIZE 10000
-
 void LanguageServer(int c, char** v) {
     serve_forever("12913");
-}
-
-const char *tokenColor(enum TokenType type) {
-    if (type == TokenIdentifier) return "yellow";
-    if (type == TokenInteger) return "green";
-    if (type == TokenString) return "red";
-    if (type == TokenFunc) return "blue";
-    return "black";
 }
 
 void route() {
@@ -33,69 +23,11 @@ void route() {
         printf("Fetch the data using `payload` variable.");
     }
 
-    GET("/highlight")
-    {
-        if (!request_header("filename")) {
-            printf("HTTP/1.1 400 Bad Request\r\n\r\n");
-            return;
-        }
-        
-        const char *filename = request_header("filename");
-        char *buffer = ReadFile(filename);
-        if (!buffer) {
-            printf("HTTP/1.1 400 Bad Request\r\n\r\n");
-            return;
-        }
-        struct TokenStream *token_stream = Lexer_Process(buffer, filename);
-
-        char output[BUFFER_SIZE];
-        int pos = 0;
-
-        output[pos] = '['; pos++;
-
-        for (int i = 0; token_stream->stream[i].type != TokenEof; i++) {
-            int cnt = sprintf(output + pos,
-                "{\"lb\":%d,\"pb\":%d,\"le\":%d,\"pe\":%d,\"cl\":\"%s\"},",
-                token_stream->stream[i].line_begin,
-                token_stream->stream[i].position_begin,
-                token_stream->stream[i].line_end,
-                token_stream->stream[i].position_end,
-                tokenColor(token_stream->stream[i].type));
-            pos += cnt;
-        }
-        output[pos] = ']'; pos++;
-
-        printf("HTTP/1.1 200 OK\r\n\r\n");
-        printf("%.*s", pos, output);
-    }
-    
     POST("/highlight")
     {
-        char *buffer = (char*)_malloc(payload_size + 1);
-        strncpy(buffer, payload, payload_size);
-        buffer[payload_size] = '\0';
-        struct TokenStream *token_stream = Lexer_Process(buffer, "");
-        _free(buffer);
-
-        char output[BUFFER_SIZE];
-        int pos = 0;
-
-        output[pos] = '['; pos++;
-
-        for (int i = 0; token_stream->stream[i].type != TokenEof; i++) {
-            int cnt = sprintf(output + pos,
-                "{\"lb\":%d,\"pb\":%d,\"le\":%d,\"pe\":%d,\"cl\":\"%s\"},",
-                token_stream->stream[i].line_begin,
-                token_stream->stream[i].position_begin,
-                token_stream->stream[i].line_end,
-                token_stream->stream[i].position_end,
-                tokenColor(token_stream->stream[i].type));
-            pos += cnt;
-        }
-        output[pos] = ']'; pos++;
-
+        const char *output = Lexer_Highlight(payload);
         printf("HTTP/1.1 200 OK\r\n\r\n");
-        printf("%.*s", pos, output);
+        printf("%.*s", (int)strlen(output), output);
     }
   
     ROUTE_END()
